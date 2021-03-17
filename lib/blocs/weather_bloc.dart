7e874c92e5f8ec927871c83704647a91ffdfa96a
khou <thao.khou@gmail.com>
2021-03-17 14:29:33 -0500

    updated dependencys as well as working on widgets and models

---
+++ lib/blocs/weather_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+import 'package:weather_app/blocs/blocs.dart';
+import 'package:weather_app/repositories/repositories.dart';
+import 'package:weather_app/models/models.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(WeatherInitial());
+
+  @override
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    if (event is WeatherRequested) {
+      yield WeatherLoadInProgress();
+      try {
+        final Weather weather = await weatherRepository.getWeather(event.city);
+        yield WeatherLoadSuccess(weather: weather);
+      } catch (_) {
+        yield WeatherLoadFailure();
+      }
+    }
+  }
+}
